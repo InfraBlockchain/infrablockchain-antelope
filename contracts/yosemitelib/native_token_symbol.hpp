96907670b3b7ed92dcc9b74497e7b9d3357f768d
bezalel <bezalel.dev@gmail.com>
2018-07-30 13:49:01 +0900

    [yosemitelib] native_token_symbol - add YOSEMITE_NATIVE_TOKEN_SYMBOL_NAME, YOSEMITE_NATIVE_TOKEN_PRECISION

--- contracts/yosemitelib/native_token_symbol.hpp
+++ contracts/yosemitelib/native_token_symbol.hpp
@@ -1,8 +1,10 @@
 /**
  * @file
- * @copyright defined in eos/LICENSE.txt
+ * @copyright defined in yosemite/LICENSE.txt
  *
- * \warning This file is machine generated. DO NOT EDIT. See native_token_symbol.hpp.in for changes.
+ * \warning This file is machine generated. DO NOT EDIT.  See native_token_symbol.hpp.in for changes.
  */
 
 #define YOSEMITE_NATIVE_TOKEN_SYMBOL S(4,DKRW)
+#define YOSEMITE_NATIVE_TOKEN_SYMBOL_NAME (YOSEMITE_NATIVE_TOKEN_SYMBOL >> 8)
+#define YOSEMITE_NATIVE_TOKEN_PRECISION 4
