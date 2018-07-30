cab7fbab2744ff98656fc337cae1ee983bc6d879
Eugene Chung <bluewolf.chung@gmail.com>
2018-07-30 11:43:00 +0900

    trivial; adjust comment

--- contracts/yosemitelib/native_token_symbol.hpp
+++ contracts/yosemitelib/native_token_symbol.hpp
@@ -1,7 +1,8 @@
-/** @file 
- *    @copyright defined in eos/LICENSE.txt 
+/**
+ * @file
+ * @copyright defined in eos/LICENSE.txt
  *
- * \warning This file is machine generated. DO NOT EDIT.  See core_symbol.hpp.in for changes.
+ * \warning This file is machine generated. DO NOT EDIT. See native_token_symbol.hpp.in for changes.
  */
 
 #define YOSEMITE_NATIVE_TOKEN_SYMBOL S(4,DKRW)
