bbf768a22c309b658bcab3547180682359849e15
bezalel <bezalel.dev@gmail.com>
2018-07-30 20:05:29 +0900

    #16 [yosemitelib] identity.hpp - identity table data format, validation/get functions

--- contracts/yosemitelib/system_accounts.hpp
+++ contracts/yosemitelib/system_accounts.hpp
@@ -7,3 +7,17 @@
 #define YOSEMITE_IDENTITY_ACCOUNT N(yx.identity)
 #define YOSEMITE_NATIVE_TOKEN_ACCOUNT N(yx.ntoken)
 #define YOSEMITE_USER_TOKEN_ACCOUNT N(yx.token)
+
+
+namespace yosemite {
+
+    bool is_system_account(const account_name account) {
+        return (account == YOSEMITE_SYSTEM_ACCOUNT
+                || account == YOSEMITE_TX_FEE_ACCOUNT
+                || account == YOSEMITE_IDENTITY_ACCOUNT
+                || account == YOSEMITE_NATIVE_TOKEN_ACCOUNT
+                || account == YOSEMITE_USER_TOKEN_ACCOUNT
+        );
+    }
+
+}
