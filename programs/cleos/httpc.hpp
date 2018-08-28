1c6cb2431655a27a99e6a546372cb32c0011107e
Eugene Chung <bluewolf.chung@gmail.com>
2018-08-28 15:27:40 +0900

    change the executable and directory names #32

--- programs/cleos/httpc.hpp
+++ programs/cleos/httpc.hpp
@@ -124,7 +124,7 @@ namespace eosio { namespace client { namespace http {
    const string wallet_remove_key = wallet_func_base + "/remove_key";
    const string wallet_create_key = wallet_func_base + "/create_key";
    const string wallet_sign_trx = wallet_func_base + "/sign_transaction";
-   const string keosd_stop = "/v1/keosd/stop";
+   const string keosd_stop = "/v1/keyos/stop";
 
    FC_DECLARE_EXCEPTION( connection_exception, 1100000, "Connection Exception" );
  }}}
