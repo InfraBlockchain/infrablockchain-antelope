89e378fa09f8aa25903908f2211cfdce62e019dc
Eugene Chung <bluewolf.chung@gmail.com>
2018-08-30 17:24:28 -0700

    keyos unlock-timeout set to 1 day #79

--- plugins/wallet_plugin/wallet_plugin.cpp
+++ plugins/wallet_plugin/wallet_plugin.cpp
@@ -27,8 +27,8 @@ void wallet_plugin::set_program_options(options_description& cli, options_descri
    cfg.add_options()
          ("wallet-dir", bpo::value<boost::filesystem::path>()->default_value("."),
           "The path of the wallet files (absolute path or relative to application data dir)")
-         ("unlock-timeout", bpo::value<int64_t>()->default_value(900),
-          "Timeout for unlocked wallet in seconds (default 900 (15 minutes)). "
+         ("unlock-timeout", bpo::value<int64_t>()->default_value(86400),
+          "Timeout for unlocked wallet in seconds (default 86400 (1 day)). "
           "Wallets will automatically lock after specified number of seconds of inactivity. "
           "Activity is defined as any wallet command e.g. list-wallets.")
          ("yubihsm-url", bpo::value<string>()->value_name("URL"),
