fad73530e00dd380a2de2f3249199533faedba86
bezalel <bezalel.dev@gmail.com>
2018-12-03 11:28:08 +0900

    #111 [StandardToken][Core] settokenmeta built-in token action message definition

---
+++ libraries/chain/include/yosemite/chain/standard_token_action_types.hpp
@@ -0,0 +1,31 @@
+/**
+ *  @file yosemite/chain/standard_token_action_types.hpp
+ *  @author bezalel@yosemitex.com
+ *  @copyright defined in yosemite/LICENSE.txt
+ */
+#pragma once
+
+#include <eosio/chain/types.hpp>
+#include <eosio/chain/symbol.hpp>
+
+namespace yosemite { namespace chain {
+
+   using namespace eosio::chain;
+
+   struct settokenmeta {
+      symbol       symbol;
+      std::string  url;
+      std::string  description;
+
+      static action_name get_name() {
+         return N(settokenmeta);
+      }
+   };
+
+   bool is_yosemite_standard_token_action(action_name action) {
+      return action == settokenmeta::get_name();
+   }
+
+} } /// yosemite::chain
+
+FC_REFLECT( yosemite::chain::settokenmeta , (symbol)(url)(description) )
