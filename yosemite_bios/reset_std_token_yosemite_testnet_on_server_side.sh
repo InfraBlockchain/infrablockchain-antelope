d84f17f0db8bd1d618e7271f43be6c26e4ca4dfa
bezalel <bezalel.dev@gmail.com>
2018-12-26 22:20:45 +0900

    #111 [StandardToken] YOSEMITE testnet bios script update (--fee-payer signature, linkauth creditissue, delegated ycardcredit.creditissue), fix YOSEMITE_CLI_TESTNET env var

--- yosemite_bios/reset_std_token_yosemite_testnet_on_server_side.sh
+++ yosemite_bios/reset_std_token_yosemite_testnet_on_server_side.sh
@@ -8,7 +8,7 @@ YOSEMITE_HOME=/mnt/yosemite-public-blockchain-git
 YOSEMITE_NODE=$YOSEMITE_HOME/build/programs/$YOSEMITE_NODE_BIN_NAME/$YOSEMITE_NODE_BIN_NAME
 YOSEMITE_NODE_LOG_FILE=/mnt/$YOSEMITE_NODE_BIN_NAME.log
 YOSEMITE_CLI="$YOSEMITE_HOME/build/programs/$YOSEMITE_CLI_BIN_NAME/$YOSEMITE_CLI_BIN_NAME --wallet-url http://127.0.0.1:8900/"
-YOSEMITE_CLI_TESTNET="$YOSEMITE_CLI -u http://testnet.yosemitelabs.org:8888 --wallet-url http://127.0.0.1:8900/"
+YOSEMITE_CLI_TESTNET="$YOSEMITE_CLI -u http://testnet.yosemitelabs.org:8888"
 YOSEMITE_KEYD=$YOSEMITE_HOME/build/programs/$YOSEMITE_KEYD_BIN_NAME/$YOSEMITE_KEYD_BIN_NAME
 YOSEMITE_KEYD_LOG_FILE=/mnt/$YOSEMITE_KEYD_BIN_NAME.log
 YOSEMITE_KEYD_WALLET_PASSWORD=PW5KH7i8ZEuVMvywMschs3TznhTfCdmgpPBGKJLUQjs6N6oQ7boZj
@@ -164,14 +164,14 @@ sleep 2
 { print_section_title "Install Standard Token ABI Interface System Contract"; } 2>/dev/null
 
 sleep 2
-$YOSEMITE_CLI set contract yx.tokenabi $YOSEMITE_CONTRACTS_DIR/yx.tokenabi/ -p yx.tokenabi@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI set contract yx.tokenabi $YOSEMITE_CONTRACTS_DIR/yx.tokenabi/ -p yx.tokenabi@active --fee-payer yosemite
 sleep 2
 
 
 { print_section_title "Install Multi-sig System Contract"; } 2>/dev/null
 
 sleep 2
-$YOSEMITE_CLI set contract yx.msig $YOSEMITE_CONTRACTS_DIR/eosio.msig/ -p yx.msig@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI set contract yx.msig $YOSEMITE_CONTRACTS_DIR/eosio.msig/ -p yx.msig@active --fee-payer yosemite
 sleep 2
 $YOSEMITE_CLI push action yosemite setpriv '["yx.msig",1]' -p yosemite@active --fee-payer yosemite
 sleep 2
@@ -180,16 +180,16 @@ sleep 2
 { print_section_title "Install Identity System Contract"; } 2>/dev/null
 
 sleep 2
-$YOSEMITE_CLI set contract yx.identity $YOSEMITE_CONTRACTS_DIR/yx.identity/ -p yx.identity@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI set contract yx.identity $YOSEMITE_CONTRACTS_DIR/yx.identity/ -p yx.identity@active --fee-payer yosemite
 sleep 2
 
 
 { print_section_title "Register Initial Identity Authorities"; } 2>/dev/null
 
-$YOSEMITE_CLI push action yosemite regidauth '["idauth.a","http://idauth-a.org",1]' -p idauth.a@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI push action yosemite regidauth '["idauth.a","http://idauth-a.org",1]' -p idauth.a@active --fee-payer yosemite
 $YOSEMITE_CLI push action yosemite authidauth '["idauth.a"]' -p yosemite@active --fee-payer yosemite
 
-$YOSEMITE_CLI push action yosemite regidauth '["idauth.b","http://idauth-b.org",1]' -p idauth.b@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI push action yosemite regidauth '["idauth.b","http://idauth-b.org",1]' -p idauth.b@active --fee-payer yosemite
 $YOSEMITE_CLI push action yosemite authidauth '["idauth.b"]' -p yosemite@active --fee-payer yosemite
 
 $YOSEMITE_CLI get table yosemite yosemite idauthority
@@ -206,16 +206,16 @@ $YOSEMITE_CLI get table yosemite yosemite producers
 
 { print_section_title "Setup Initial System Tokens"; } 2>/dev/null
 
-$YOSEMITE_CLI push action systoken.a settokenmeta '["2,DKRW","http://sysdepo-a.org","system depository a"]' -p systoken.a@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI push action systoken.a settokenmeta '["2,DKRW","http://sysdepo-a.org","system depository a"]' -p systoken.a@active --fee-payer yosemite
 
-$YOSEMITE_CLI push action systoken.a issue '["systoken.a","systoken.a","100000.00 DKRW","issue systoken.a"]' -p systoken.a@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI push action systoken.a issue '["systoken.a","systoken.a","100000.00 DKRW","issue systoken.a"]' -p systoken.a@active --fee-payer yosemite
 
 $YOSEMITE_CLI get token info systoken.a
 $YOSEMITE_CLI get token balance systoken.a systoken.a
 
-$YOSEMITE_CLI push action systoken.b settokenmeta '["2,DKRWB","http://sysdepo-b.org","system depository b"]' -p systoken.b@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI push action systoken.b settokenmeta '["2,DKRWB","http://sysdepo-b.org","system depository b"]' -p systoken.b@active --fee-payer yosemite
 
-$YOSEMITE_CLI push action systoken.b issue '["systoken.b","systoken.b","200000.00 DKRWB","issue systoken.b"]' -p systoken.b@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI push action systoken.b issue '["systoken.b","systoken.b","200000.00 DKRWB","issue systoken.b"]' -p systoken.b@active --fee-payer yosemite
 
 $YOSEMITE_CLI get token info systoken.b
 $YOSEMITE_CLI get token balance systoken.b systoken.b
@@ -226,30 +226,47 @@ $YOSEMITE_CLI push action yosemite addsystoken '["systoken.b",10000]' -p yosemit
 
 $YOSEMITE_CLI get systoken list
 
+$YOSEMITE_CLI get systoken balance systoken.a
+$YOSEMITE_CLI get systoken balance systoken.b
+
 
 { print_section_title "Setup Initial Transaction Fees for Built-in Actions"; } 2>/dev/null
 
-$YOSEMITE_CLI push action yosemite settxfee '["yosemite","newaccount",50000,1]' -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI push action yosemite settxfee '["","",100000,1]' -p yosemite@active --fee-payer yosemite
 $YOSEMITE_CLI push action yosemite settxfee '["","issue",5000,1]' -p yosemite@active --fee-payer yosemite
 $YOSEMITE_CLI push action yosemite settxfee '["","transfer",10000,1]' -p yosemite@active --fee-payer yosemite
 $YOSEMITE_CLI push action yosemite settxfee '["","redeem",20000,1]' -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI push action yosemite settxfee '["yosemite","newaccount",50000,1]' -p yosemite@active --fee-payer yosemite
 
+$YOSEMITE_CLI push action yosemite settxfee '["yx.identity","setidinfo",5000,1]' -p yosemite@active --fee-payer yosemite
+sleep 1
+$YOSEMITE_CLI get txfee list -L "yx.identity" -U "yx.identity"
+$YOSEMITE_CLI push action yosemite unsettxfee '["yx.identity","setidinfo"]' -p yosemite@active --fee-payer yosemite
+sleep 1
+$YOSEMITE_CLI get txfee list -L "yx.identity" -U "yx.identity"
 $YOSEMITE_CLI push action yosemite settxfee '["yx.identity","setidinfo",0,1]' -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI get txfee list -L "yx.identity" -U "yx.identity"
 
+$YOSEMITE_CLI get txfee item yosemite newaccount
+$YOSEMITE_CLI get txfee item "" transfer
+$YOSEMITE_CLI get txfee item "" ""
+$YOSEMITE_CLI get txfee list -L "" -U 1
+$YOSEMITE_CLI get txfee list -L yosemite -U yosemite -l 50
+$YOSEMITE_CLI get txfee list
 
 { print_section_title "Deploy YOSEMITE Fiat Stable Coin as a System Token"; } 2>/dev/null
 
 $YOSEMITE_CLI wallet import --private-key YPV_5K9aJSJyMhNLWvAtYm6x3ZYL13eaToVeT6JjQ1ABkQd383gXHFa
-$YOSEMITE_CLI create account yosemite ysmt.dkrw YOS568YZYS8i2Gr6gbJE9CEEsyaZFfRXzVS9BNw9FfCLr9Yccpg7o -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI create account yosemite ysmt.dkrw YOS568YZYS8i2Gr6gbJE9CEEsyaZFfRXzVS9BNw9FfCLr9Yccpg7o --fee-payer yosemite
 
 sleep 2
-$YOSEMITE_CLI set contract ysmt.dkrw $YOSEMITE_CONTRACTS_DIR/yosemitex.fiat.stable.token/ -p ysmt.dkrw@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI set contract ysmt.dkrw $YOSEMITE_CONTRACTS_DIR/yosemitex.fiat.stable.token/ -p ysmt.dkrw@active --fee-payer yosemite
 sleep 2
 
-$YOSEMITE_CLI push action ysmt.dkrw settokenmeta '["2,DKRW","https://yosemitex.com","Yosemite X Digital KRW Token"]' -p ysmt.dkrw@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI push action ysmt.dkrw settokenmeta '["2,DKRW","https://yosemitex.com","Yosemite X Digital KRW Token"]' -p ysmt.dkrw@active --fee-payer yosemite
 
 $YOSEMITE_CLI push action yx.identity setidinfo "{\"account\":\"ysmt.dkrw\", \"identity_authority\":\"idauth.a\", \"type\":$(echo 'ibase=2; 0' | bc), \"kyc\":$(echo 'ibase=2; 1111' | bc), \"state\":$(echo 'ibase=2; 0' | bc), \"data\":\"ysmt.dkrw\"}" -p idauth.a@active --fee-payer idauth.a
-$YOSEMITE_CLI push action ysmt.dkrw issue '["ysmt.dkrw","ysmt.dkrw","10000000.00 DKRW",""]' -p ysmt.dkrw@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI push action ysmt.dkrw issue '["ysmt.dkrw","ysmt.dkrw","10000000.00 DKRW",""]' -p ysmt.dkrw@active --fee-payer yosemite
 
 $YOSEMITE_CLI push action yosemite settxfee '["ysmt.dkrw","issue",0,1]' -p yosemite@active --fee-payer yosemite
 $YOSEMITE_CLI push action yosemite settxfee '["ysmt.dkrw","redeem",5000,1]' -p yosemite@active --fee-payer yosemite
@@ -263,7 +280,7 @@ $YOSEMITE_CLI push action yosemite addsystoken '["ysmt.dkrw",10000]' -p yosemite
 $YOSEMITE_CLI push action systoken.a issue '["systoken.a","idauth.a","1000000.00 DKRW",""]' -p systoken.a@active -fee-payer systoken.a
 
 $YOSEMITE_CLI push action systoken.a transfer '["systoken.a","idauth.a","idauth.b","5000.00 DKRW","transfer tag"]' -p idauth.a@active --fee-payer idauth.a
-$YOSEMITE_CLI push action systoken.a transfer '["systoken.a","idauth.a","idauth.b","3000.00 DKRW","transfer tag 2"]' -p idauth.a@active -p idauth.b@active --fee-payer idauth.b
+$YOSEMITE_CLI push action systoken.a transfer '["systoken.a","idauth.a","idauth.b","3000.00 DKRW","transfer tag 2"]' -p idauth.a@active --fee-payer idauth.b
 
 $YOSEMITE_CLI get token balance systoken.a idauth.a
 $YOSEMITE_CLI get token balance systoken.a idauth.b
@@ -275,9 +292,9 @@ $YOSEMITE_CLI wallet import --private-key YPV_5KTdP4VyuhZ1ba3sX8JvHjKXgcTFUA75QH
 $YOSEMITE_CLI wallet import --private-key YPV_5KDKMejP2AZFGCMxLh67t7417coEgUu7PfQZk7ufuYA4wTBiNC4
 $YOSEMITE_CLI wallet import --private-key YPV_5Jj4ryu8TY1cVhiLvPkBHKiwMAXV18WqJM97h8hfsBUt5sMJE3r
 
-$YOSEMITE_CLI create account idauth.a useraccounta YOS7y8FkTHo58ZvrMa6yrTpo3D2Y2AA4dMAZzQ8aZY6R6geZSDZ75 -p idauth.a@active --fee-payer idauth.a
-$YOSEMITE_CLI create account idauth.a useraccountb YOS8ZFsmfpevP8pKPMXuQMNB7P9KJoKzZwwzu2uww9VrRx7E7NUQj -p idauth.a@active --fee-payer idauth.a
-$YOSEMITE_CLI create account idauth.a useraccountc YOS5ubmvsnjHviACtfc9SwGbY7SprqTu1P5GQeDfwfZmhCq6aR7GH -p idauth.a@active --fee-payer idauth.a
+$YOSEMITE_CLI create account idauth.a useraccounta YOS7y8FkTHo58ZvrMa6yrTpo3D2Y2AA4dMAZzQ8aZY6R6geZSDZ75 --fee-payer idauth.a
+$YOSEMITE_CLI create account idauth.a useraccountb YOS8ZFsmfpevP8pKPMXuQMNB7P9KJoKzZwwzu2uww9VrRx7E7NUQj --fee-payer idauth.a
+$YOSEMITE_CLI create account idauth.a useraccountc YOS5ubmvsnjHviACtfc9SwGbY7SprqTu1P5GQeDfwfZmhCq6aR7GH --fee-payer idauth.a
 
 $YOSEMITE_CLI push action yx.identity setidinfo "{\"account\":\"useraccounta\", \"identity_authority\":\"idauth.a\", \"type\":$(echo 'ibase=2; 0' | bc), \"kyc\":$(echo 'ibase=2; 1111' | bc), \"state\":$(echo 'ibase=2; 0' | bc), \"data\":\"qwefewff2f\"}" -p idauth.a@active --fee-payer idauth.a
 $YOSEMITE_CLI push action yx.identity setidinfo "{\"account\":\"useraccountb\", \"identity_authority\":\"idauth.a\", \"type\":$(echo 'ibase=2; 0' | bc), \"kyc\":$(echo 'ibase=2; 1111' | bc), \"state\":$(echo 'ibase=2; 0' | bc), \"data\":\"wef2ouih23\"}" -p idauth.a@active --fee-payer idauth.a
@@ -293,7 +310,6 @@ $YOSEMITE_CLI get token balance ysmt.dkrw useraccountb
 $YOSEMITE_CLI push action ysmt.dkrw transfer '["ysmt.dkrw","useraccounta","useraccountb","10000.00 DKRW","transfer tag"]' -p useraccounta@active --fee-payer useraccounta
 
 
-
 $YOSEMITE_CLI push action systoken.a issue '["systoken.a","useraccounta","100000.00 DKRW",""]' -p systoken.a@active --fee-payer systoken.a
 $YOSEMITE_CLI push action systoken.b issue '["systoken.b","useraccounta","100000.00 DKRWB",""]' -p systoken.b@active --fee-payer systoken.b
 
@@ -302,22 +318,25 @@ $YOSEMITE_CLI push action systoken.a issue '["systoken.a","useraccountc","300000
 
 $YOSEMITE_CLI get token balance systoken.a useraccounta
 $YOSEMITE_CLI get token balance systoken.b useraccounta
-#$YOSEMITE_CLI get systoken balance useraccounta
+$YOSEMITE_CLI get systoken balance useraccounta
 
 $YOSEMITE_CLI get token balance systoken.a useraccountb
 $YOSEMITE_CLI get token balance systoken.a useraccountc
 
 $YOSEMITE_CLI push action systoken.a transfer '["systoken.a","useraccounta","useraccountb","99950.00 DKRW","transfer tag"]' -p useraccounta@active --fee-payer useraccounta
 
+$YOSEMITE_CLI get systoken balance useraccounta
+$YOSEMITE_CLI get systoken balance useraccountb
+$YOSEMITE_CLI get systoken balance useraccountc
 
 
 { print_section_title "YOSEMITE Credit Token Test"; } 2>/dev/null
 
 $YOSEMITE_CLI wallet import --private-key YPV_5KKMcPu86p6vbsuAozTT9vLxUT4GeZFqSWrbgok1LjpthJEB5Qe
-$YOSEMITE_CLI create account yosemite ycardcredit YOS7mK7vvTTCDzEr8gtMjrMsbWA25UsS3WmV8WRoqC5gSMh9JLcQM -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI create account yosemite ycardcredit YOS7mK7vvTTCDzEr8gtMjrMsbWA25UsS3WmV8WRoqC5gSMh9JLcQM --fee-payer yosemite
 
 sleep 2
-$YOSEMITE_CLI set contract ycardcredit $YOSEMITE_CONTRACTS_DIR/yosemitex.credit.token/ -p ycardcredit@active -p yosemite@active --fee-payer yosemite
+$YOSEMITE_CLI set contract ycardcredit $YOSEMITE_CONTRACTS_DIR/yosemitex.credit.token/ -p ycardcredit@active --fee-payer yosemite
 sleep 2
 
 $YOSEMITE_CLI push action ycardcredit settokenmeta '["2,CRD","https://yosemitecardx.com","Yosemite Card X Credit Token"]' -p ycardcredit@active -p yosemite@active --fee-payer yosemite
@@ -337,13 +356,14 @@ $YOSEMITE_CLI push action ycardcredit creditlimit '["useraccounta","50000.00 CRD
 $YOSEMITE_CLI get table -L useraccounta -l 1 ycardcredit ycardcredit creditoffer
 
 ## useraccounta issues credit tokens and deposits to ycard service
-$YOSEMITE_CLI push action ycardcredit creditissue '["useraccounta","50000.00 CRD","issue credit token by user"]' -p useraccounta@active -p ycardcredit@active --fee-payer ycardcredit
+$YOSEMITE_CLI push action yosemite updateauth '{"account":"useraccounta","permission":"creditissue","parent":"active","auth":{"threshold":1,"keys":[],"waits":[],"accounts":[{"weight":1,"permission":{"actor":"ycardcredit","permission":"active"}}]}}' -p useraccounta@active --fee-payer ycardcredit
+$YOSEMITE_CLI push action yosemite linkauth '["useraccounta","ycardcredit","creditissue","creditissue"]' -p useraccounta@active --fee-payer ycardcredit
+
+$YOSEMITE_CLI push action ycardcredit creditissue '["useraccounta","ycardcredit","50000.00 CRD","issue credit token by user, and deposit to credit service"]' -p useraccounta@creditissue --fee-payer ycardcredit
+#$YOSEMITE_CLI push action ycardcredit transfer '["ycardcredit","useraccounta","ycardcredit","50000.00 CRD","deposit credit to service account for anonymous tx"]' -p useraccounta@active --fee-payer ycardcredit
 
 $YOSEMITE_CLI get table -L useraccounta -l 1 ycardcredit ycardcredit creditoffer
 $YOSEMITE_CLI get token balance ycardcredit useraccounta
-
-$YOSEMITE_CLI push action ycardcredit transfer '["ycardcredit","useraccounta","ycardcredit","50000.00 CRD","deposit credit to service account for anonymous tx"]' -p useraccounta@active -p ycardcredit@active --fee-payer ycardcredit
-
 $YOSEMITE_CLI get token balance ycardcredit ycardcredit
 
 ## anonymous payment transaction to merchant useraccountb
@@ -359,12 +379,11 @@ $YOSEMITE_CLI push action ycardcredit redeem '["30000.00 CRD","burn unused usera
 
 $YOSEMITE_CLI push action ycardcredit creditredeem '["useraccounta","50000.00 CRD","reset useraccounta credit offering"]' -p ycardcredit@active --fee-payer ycardcredit
 
-$YOSEMITE_CLI push action ycardcredit creditissue '["useraccounta","50000.00 CRD","issue credit token by user"]' -p useraccounta@active -p ycardcredit@active --fee-payer ycardcredit
-
-$YOSEMITE_CLI push action ycardcredit transfer '["ycardcredit","useraccounta","ycardcredit","50000.00 CRD","deposit credit to service account for anonymous tx"]' -p useraccounta@active -p ycardcredit@active --fee-payer ycardcredit
+$YOSEMITE_CLI push action ycardcredit creditissue '["useraccounta","ycardcredit","50000.00 CRD","issue credit token by user, and deposit to credit service"]' -p useraccounta@creditissue --fee-payer ycardcredit
+#$YOSEMITE_CLI push action ycardcredit transfer '["ycardcredit","useraccounta","ycardcredit","50000.00 CRD","deposit credit to service account for anonymous tx"]' -p useraccounta@active --fee-payer ycardcredit
 
 ## merchant(useraccountb) redeems credit tokens and withdraws fiat
-$YOSEMITE_CLI push action ycardcredit transfer '["ycardcredit","useraccountb","ycardcredit","20000.00 CRD","request redeeming credit token"]' -p useraccountb@active -p ycardcredit@active --fee-payer ycardcredit
+$YOSEMITE_CLI push action ycardcredit transfer '["ycardcredit","useraccountb","ycardcredit","20000.00 CRD","request redeeming credit token"]' -p useraccountb@active --fee-payer ycardcredit
 
 $YOSEMITE_CLI push action ysmt.dkrw transfer '["ysmt.dkrw","ycardcredit","useraccountb","20000.00 DKRW","redeeming CRD to DKRW"]' -p ycardcredit@active --fee-payer ycardcredit
 
@@ -373,5 +392,5 @@ $YOSEMITE_CLI push action ycardcredit redeem '["20000.00 CRD","burn redeemed cre
 $YOSEMITE_CLI get token balance ycardcredit useraccountb
 $YOSEMITE_CLI get token balance ysmt.dkrw useraccountb
 
-$YOSEMITE_CLI push action ysmt.dkrw transfer '["ysmt.dkrw","useraccountb","ysmt.dkrw","20000.00 DKRW","request redeeming DKRW to fiat"]' -p useraccountb@active -p ycardcredit@active --fee-payer ycardcredit
+$YOSEMITE_CLI push action ysmt.dkrw transfer '["ysmt.dkrw","useraccountb","ysmt.dkrw","20000.00 DKRW","request redeeming DKRW to fiat"]' -p useraccountb@active --fee-payer ycardcredit
 $YOSEMITE_CLI push action ysmt.dkrw redeem '["20000.00 DKRW","redeem DKRW to fiat"]' -p ysmt.dkrw@active --fee-payer ysmt.dkrw
