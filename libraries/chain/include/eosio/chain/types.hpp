6d753263325a33e1a154ee5b3cbc6cb458867771
bezalel <bezalel.dev@gmail.com>
2021-09-30 19:03:26 +0900

    [StandardToken][Core] system token list object / infrablockchain global property chainbase schema

--- libraries/chain/include/eosio/chain/types.hpp
+++ libraries/chain/include/eosio/chain/types.hpp
@@ -261,9 +261,11 @@ namespace eosio { namespace chain {
       OBJECT_TYPE_COUNT, ///< Sentry value which contains the number of different object types
 
       INFRABLOCKCHAIN_OBJECT_TYPE_DELIMITER = 1000,
+      infrablockchain_global_property_object_type,
       infrablockchain_token_meta_object_type,
       infrablockchain_token_balance_object_type,
-      infrablockchain_transaction_fee_table_object_type
+      infrablockchain_transaction_fee_table_object_type,
+      infrablockchain_received_transaction_votes_object_type
    };
 
    /**
