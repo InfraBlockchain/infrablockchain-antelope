bd91fbc73646def31340952874b5d410e639c71c
bezalel <bezalel.dev@gmail.com>
2021-10-02 15:08:11 +0900

    infrablockchain_global_property_object - snapshot_row_traits impl.

--- libraries/chain/include/infrablockchain/chain/infrablockchain_global_property_object.hpp
+++ libraries/chain/include/infrablockchain/chain/infrablockchain_global_property_object.hpp
@@ -7,6 +7,8 @@
 
 #include <eosio/chain/database_utils.hpp>
 #include <eosio/chain/multi_index_includes.hpp>
+#include <eosio/chain/snapshot.hpp>
+
 #include <infrablockchain/chain/system_token_list.hpp>
 #include <infrablockchain/chain/received_transaction_votes_object.hpp>
 
@@ -32,8 +34,39 @@ namespace infrablockchain { namespace chain {
       >
    >;
 
+   struct snapshot_infrablockchain_global_property_object {
+      system_token_list              system_token_list;
+      tx_votes_sum_weighted_type     total_tx_votes_weighted = 0.0;
+      tx_votes_sum_type              total_tx_votes = 0;
+      uint64_t                       reserved1 = 0;
+      uint64_t                       reserved2 = 0;
+   };
+
 } } /// infrablockchain::chain
+
+namespace eosio { namespace chain {
+
+   namespace detail {
+
+      using namespace infrablockchain::chain;
+
+      template<>
+      struct snapshot_row_traits<infrablockchain_global_property_object> {
+         using value_type = infrablockchain_global_property_object;
+         using snapshot_type = snapshot_infrablockchain_global_property_object;
+
+         static snapshot_infrablockchain_global_property_object to_snapshot_row( const infrablockchain_global_property_object& value,
+                                                                                 const chainbase::database& db );
+
+         static void from_snapshot_row( snapshot_infrablockchain_global_property_object&& row,
+                                        infrablockchain_global_property_object& value,
+                                        chainbase::database& db );
+      };
+   }
+
+} } /// eosio::chain
 
 CHAINBASE_SET_INDEX_TYPE(infrablockchain::chain::infrablockchain_global_property_object, infrablockchain::chain::infrablockchain_global_property_multi_index)
 
 FC_REFLECT(infrablockchain::chain::infrablockchain_global_property_object, (system_token_list)(total_tx_votes_weighted)(total_tx_votes)(reserved1)(reserved2))
+FC_REFLECT(infrablockchain::chain::snapshot_infrablockchain_global_property_object, (system_token_list)(total_tx_votes_weighted)(total_tx_votes)(reserved1)(reserved2))
